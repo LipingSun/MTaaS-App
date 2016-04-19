2e5038af91343dd018e3c89604af19e5660435d5
LipingSun <liping.sun@sjsu.edu>
2016-04-19 12:22:48 -0700

    implement connect and disconnect

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -6,40 +6,71 @@ import 'rxjs/add/operator/map'
 
 
 @Page({
-  templateUrl: 'build/pages/home/home.html'
+    templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(public nav:NavController, public http:Http) {
+    constructor(public nav:NavController, public http:Http) {
+    }
 
-  }
+    connected = false;
+    // deviceUrl = "http://localhost:3000/api/v1/devices";
+    deviceUrl = "http://mtaas-controller-elb-1154311609.us-west-2.elb.amazonaws.com:3000/api/v1/devices";
 
-  connect() {
-    // let deviceUrl = "http://192.168.1.118:3000/api/v1/devices";
-    let deviceUrl = "http://mtaas-controller-elb-1154311609.us-west-2.elb.amazonaws.com:3000/api/v1/devices";
-    let deviceInfo = JSON.stringify(Device.device);
-    let alert = Alert.create({
-      buttons: ['OK']
-    });
-    console.log(deviceInfo);
+    connect() {
+        let deviceInfo = JSON.stringify(Device.device);
+        let alert = Alert.create({
+            buttons: ['OK']
+        });
+        console.log(deviceInfo);
 
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
 
-    this.http.post(deviceUrl, deviceInfo, {headers: headers})
-        // .map(res => res.json())
-        .subscribe(
-            res => {
-              if (res.status == 201) {
-                alert.setTitle("Connected");
-                alert.setSubTitle(deviceInfo);
-                this.nav.present(alert);
-              }
-            },
-            err => {
-              alert.setTitle("Failed");
-              alert.setSubTitle(err.message);
-              this.nav.present(alert);
-            }
-        );
-  }
+        this.http.post(this.deviceUrl, deviceInfo, {headers: headers})
+            // .map(res => res.json())
+            .subscribe(
+                res => {
+                    if (res.status == 201) {
+                        alert.setTitle("Connected");
+                        alert.setSubTitle(deviceInfo);
+                        this.nav.present(alert);
+                        this.connected = true;
+                    }
+                },
+                err => {
+                    alert.setTitle("Failed");
+                    alert.setSubTitle(err.message);
+                    this.nav.present(alert);
+                }
+            );
+    }
+
+    disconnect() {
+        let alert = Alert.create({
+            buttons: ['OK']
+        });
+
+        // let headers = new Headers();
+        // headers.append('Content-Type', 'application/json');
+
+        console.log(this.deviceUrl + Device.device.uuid);
+
+        this.http.delete(this.deviceUrl + '/' + Device.device.uuid)
+            // .map(res => res.json())
+            .subscribe(
+                res => {
+                    if (res.status == 200) {
+                        alert.setTitle("Disconnected");
+                        // alert.setSubTitle(deviceInfo);
+                        this.nav.present(alert);
+                        this.connected = false;
+                    }
+                },
+                err => {
+                    alert.setTitle("Failed");
+                    alert.setSubTitle(err.message);
+                    this.nav.present(alert);
+                }
+            );
+    }
 }
